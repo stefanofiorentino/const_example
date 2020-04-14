9d475ff5e8d2b67f3584c0c33fefa982bdc72a6e
Stefano Fiorentino <stefano.fiore84@gmail.com>
2020-04-14 12:44:55 +0200

    const member functions
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 
 struct base {
-    void hello() {
-        std::cout << "hello" << std::endl;
+    virtual ~base();
+
+    virtual void hello() {
+        std::cout << "Hello, I'm base" << std::endl;
     }
 
-    void hello() const {
-        std::cout << "const hello" << std::endl;
+    virtual void hello() const {
+        std::cout << "Hello, I'm const base" << std::endl;
+    }
+};
+
+base::~base() = default;
+
+struct derived : public base {
+    void hello() override {
+        std::cout << "Hello, I'm derived" << std::endl;
+    }
+
+    void hello() const override {
+        std::cout << "Hello, I'm const derived" << std::endl;
     }
 };
 
 int main() {
-    {
-        base b;
-        b.hello();
-    }
-    {
-        const base cb;
-        cb.hello();
-    }
+    base b;
+    b.hello();
+
+    derived d;
+    d.hello();
+
+    const base cb;
+    cb.hello();
+
+    const derived cd;
+    cd.hello();
+
     return 0;
 }
